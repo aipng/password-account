925be16e0444dab3978e788a1215728329c1471e
Karel Horsky <karel.horsky@centrum.cz>
2017-08-04 15:28:38 +0200

    remove RobotLoader from tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,21 +2,9 @@
 
 declare(strict_types = 1);
 
-use Nette\Loaders\RobotLoader;
 use Tester\Environment;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-define('TMP_DIR', __DIR__ . '/../temp');
-
 Environment::setup();
 date_default_timezone_set('Europe/Prague');
-
-$loader = new RobotLoader;
-$loader
-	->setTempDirectory(TMP_DIR)
-	->setAutoRefresh()
-	->addDirectory(__DIR__)
-	->addDirectory(__DIR__ . '/../src');
-
-return $loader->register();
